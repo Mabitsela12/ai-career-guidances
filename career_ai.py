8bf282fe0ad9709cfd2364a956fd22349aaaeed9
Mabitsela Mathipa <166811888+Mabitsela12@users.noreply.github.com>
2024-09-10 10:37:28 +0200

    Update career_ai.py

--- career_ai.py
+++ career_ai.py
@@ -197,7 +197,7 @@ def main():
 
     def main():
     # Upload or URL of your picture
-    picture_path = ""  # Change this to the path of your picture file
+    picture_path = "code image.jpg"  # Change this to the path of your picture file
     st.image(picture_path, use_column_width=True)  # Display your picture
 
     st.subheader(texts["multilingual_support_header"])
